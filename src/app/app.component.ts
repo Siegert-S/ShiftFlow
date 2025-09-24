3d8f353f3b81174d2acab3552a8184de65e4c360
Sascha Siegert <s.siegert85@googlemail.com>
2025-09-24 13:00:37 +0200

    add user-list & menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from "./components/login/login.component";
 import { NgClass } from  '@angular/common';
 import { HeaderComponent } from "./components/header/header.component";
+import { MenuComponent } from "./components/menu/menu.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent, NgClass, HeaderComponent],
+  imports: [RouterOutlet, LoginComponent, NgClass, HeaderComponent, MenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
